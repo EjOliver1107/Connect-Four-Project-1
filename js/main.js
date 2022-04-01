90708673dc09ae3cf7d1528ee18590d55cbef6f3
K1neticz <ejoliver@ymail.com>
2022-04-01 15:41:10 -0700

    Adds constants, variables, and cached element refrences

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Bogos Binted?")
+/*----- constants -----*/
+const LOOKUP = {
+    '1': '#5af287',
+    '-1': '#C600EB',
+    'null': white
+};
+
+
+/*----- app's state (variables) -----*/
+let board;
+let turn;
+let winner;
+
+/*----- cached element references -----*/
+const checkers = document.querySelectorAll('td div');
+const message = document.querySelector('footer h1');
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/

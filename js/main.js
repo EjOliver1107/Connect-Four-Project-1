6a1593e221f08d7525d47d99d99059f2c3918c04
K1neticz <ejoliver@ymail.com>
2022-04-03 20:31:22 -0700

    Attempts to initialize, render, and handle player moves

--- js/main.js
+++ js/main.js
@@ -15,11 +15,30 @@ let winner;
 /*----- cached element references -----*/
 const checkers = document.querySelectorAll('td div');
 const message = document.querySelector('footer h1');
-
+const rows = document.querySelectorAll('tr');
 
 /*----- event listeners -----*/
-
+document.querySelector('table').addEventListener('click', handleMove);
 
 
 /*----- functions -----*/
 
+init();
+
+function init() {
+    let idx = parseInt(evt.target.id.replace('cl', ''));
+    board[idx] = null;
+}
+
+function render() {
+    board.forEach(function(cl, idx) {
+        checkers[idx].innerText = LOOKUP[cl];
+    });
+}
+function handleMove(evt) {
+    let idx = parseInt(evt.target.id.replace('cl', ''));
+    board[idx] = turn;
+    turn *=-1;
+
+    render();
+}

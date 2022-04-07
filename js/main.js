7bf519dc1de2841d52fd912cd72c63ecc995c2de
K1neticz <ejoliver@ymail.com>
2022-04-07 14:45:59 -0700

    Fixes win conditions and adds tie function

--- js/main.js
+++ js/main.js
@@ -12,12 +12,14 @@ let board;
 let turn;
 let winner;
 let result;
+let tieArray;
 /*----- cached element references -----*/
 const slots = [...document.querySelectorAll('td')];
 const currentTurn = document.querySelector('aside > div');
 const rows = document.querySelectorAll('tr');
 const markerEls = [...document.querySelectorAll('#markers> div')];
 const message = document.querySelector('aside > h3');
+
 /*----- event listeners -----*/
 document.getElementById('markers').addEventListener('click', handleMove);
 document.querySelector('button').addEventListener('click', init);
@@ -35,60 +37,82 @@ function init() {
         [0, 0, 0, 0, 0, 0],
     ];
     turn = 1;
+    tieArray= [];
+    render();
    
-    render();
-    
-}
+  } 
 
-function render() {
+  function render() {
     board.forEach(function(colArr, colIdx){
-        // console.log('colArr', colArr);
-        colArr.forEach(function(cellVal, rowIdx) {
-            // console.log('cellVall', cellVal);
-            // console.log('rowIdx', rowIdx);
-            const cellEl = document.getElementById(`cl${colIdx}r${rowIdx}`)
-            // console.log('cellEl', cellEl);
-            cellEl.style.backgroundColor = LOOKUP[cellVal];
-            
-        });
+      // console.log('colArr', colArr);
+      colArr.forEach(function(cellVal, rowIdx) {
+        // console.log('cellVall', cellVal);
+        // console.log('rowIdx', rowIdx);
+        const cellEl = document.getElementById(`cl${colIdx}r${rowIdx}`)
+        // console.log('cellEl', cellEl);
+        cellEl.style.backgroundColor = LOOKUP[cellVal];
+        
+      });
     });
-    if (winner === 'T') {
-      message.innerHTML = 'No one Wins!'
-    } else if (winner) {
-      message.innerHTML = `Player ${LOOKUP[winner].toUpperCase()}`;
-    } else {
-        currentTurn.style.backgroundColor = LOOKUP[turn];
-    }
-
-}
-function handleMove(evt) {
-    const colIdx = markerEls.indexOf(evt.target);
-    if (colIdx === -1) return;
-    const colArr = board[colIdx];
-    // console.log(colIdx);
-    // console.log(colArr);
-    const rowIdx = colArr.indexOf(0);
-    colArr[rowIdx] = turn;
-    turn *= -1;
-    winner = checkWin();
-    render();
     
-}
-function checkWin() {
-    for(let i=0; i < board.length -4; i++){
-      for(let j=0; j < board[i].length -4; j++){
-        if(board[i] [j]=== turn && board[i][j+1] === turn && board[i][j+2] && board[i][j+3]){
-          winner = true
-        } else if(board[i][j]=== turn && board[i+1][j] === turn && board[i+2][j] === turn && board[i+3][j]){
-          winner = true
-        }else if(board[i][j]=== turn && board[i+1][j+1] === turn && board[i+2][j+2] === turn && board[i+3][j+3]){
-          winner = true
-        }else if(board[i][j]=== turn && board[i+1][j-1] === turn && board[i+2][j-2] === turn && board[i+3][j-3]){
-          winner = true
-      } else {
-        return 'T';
-      }
-
-    }
   }
-  };
+  function getWinner(colIdx, rowIdx) {
+    return checkVertWin(colIdx, rowIdx) || checkHorzWin(colIdx, rowIdx);
+  }
+
+  function handleMove(evt) {
+    const colIdx = markerEls.indexOf(evt.target);
+        if (colIdx === -1) return;
+        const colArr = board[colIdx];
+        // console.log(colIdx);
+        // console.log(colArr);
+        const rowIdx = colArr.indexOf(0);
+        colArr[rowIdx] = turn;
+        turn *= -1;
+        render();
+        getWinner(colIdx, rowIdx);
+        tieArray.push(1);
+        tieCheck();
+      }
+      
+      function checkVertWin(colIdx, rowIdx) {
+  const player = board[colIdx][rowIdx];
+  let count = 1; 
+  //count up
+  let idx = rowIdx + 1; // initialize to one above 
+  while (idx < board[idx].length && board[colIdx][idx] === player) {
+    count++;
+    idx++;
+  }
+  idx = rowIdx - 1; // initialize to one above 
+  while (idx >= 0 && board[colIdx][idx] === player) {
+    count++;
+    idx--;
+  }
+  return count === 4 ? winner = true : null; 
+}
+
+
+//opposite
+function checkHorzWin(colIdx, rowIdx) {
+  const player = board[colIdx][rowIdx];
+  let count = 1; 
+  //count right
+  let idx = colIdx + 1; // initialize to one above 
+  while (idx < board.length && board[idx][rowIdx] === player) {
+    count++;
+    idx++;
+  }
+  idx = colIdx - 1; // initialize to one above 
+  while (idx >= 0 && board[idx][rowIdx] === player) {
+    count++;
+    idx--;
+  }
+  return count >= 4 ? winner = true: null;
+}
+function tieCheck() {
+ 
+  if (tieArray.length === 42) {     
+      message.innerHTML = 'There has been a stalemate!';
+  }
+}
